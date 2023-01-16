ba1d3f7d8998ece5f4c313d13a5bac8b6d139edb
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-16 15:56:23 +0200

    Add all path for position view in task_manager/urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import index, TaskTypeListView, TaskTypeCreateView, TaskTypeUpdateView, TaskTypeDeleteView
+from .views import (index,
+                    TaskTypeListView,
+                    TaskTypeCreateView,
+                    TaskTypeUpdateView,
+                    TaskTypeDeleteView,
+                    PositionListView,
+                    PositionUpdateView,
+                    PositionDeleteView,
+                    PositionCreateView)
 
 urlpatterns = [
     path("", index, name="index"),
@@ -7,6 +15,10 @@ urlpatterns = [
     path("task_type/create/", TaskTypeCreateView.as_view(), name="task_type_create_"),
     path("task_type/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task_type_update"),
     path("task_type/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task_type_delete"),
+    path("position/", PositionListView.as_view(), name="position_list"),
+    path("position/create/", PositionCreateView.as_view(), name="position_create_"),
+    path("position/<int:pk>/update/", PositionUpdateView.as_view(), name="position_update"),
+    path("position/<int:pk>/delete/", PositionDeleteView.as_view(), name="task_type_delete"),
 ]
 
 app_name = "task_manager"
