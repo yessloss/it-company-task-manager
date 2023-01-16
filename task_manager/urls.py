86c6c715ee0d33f99c58775380e874c17785b574
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-16 15:43:16 +0200

    Add all path for task type view in task_manager/urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index
-
+from .views import index, TaskTypeListView, TaskTypeCreateView, TaskTypeUpdateView, TaskTypeDeleteView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("task_type/", TaskTypeListView.as_view(), name="task_type_list"),
+    path("task_type/create/", TaskTypeCreateView.as_view(), name="task_type_create_"),
+    path("task_type/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task_type_update"),
+    path("task_type/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task_type_delete"),
 ]
 
 app_name = "task_manager"
