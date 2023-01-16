d75b66ed75c61a3154bee14390b10002c2241633
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-16 12:08:15 +0200

    Add model Worker to the models in the task_manager

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,11 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
 
+
 class Position(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
 
 
 
