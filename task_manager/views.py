2c8a1b80775e21315a4903db3bacb5a81db1e2db
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-16 13:25:55 +0200

    Add view(index) for home screen

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import TaskType, Task, Position, Worker
+
+def index(request):
+    num_task_types = TaskType.objects.count()
+    num_tasks = Task.objects.count()
+    num_positions = Position.objects.count()
+    num_workers = Worker.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_task_types": num_task_types,
+        "num_tasks": num_tasks,
+        "num_positions": num_positions,
+        "num_workers" : num_workers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_manager/index.html", context=context)
+
+
