ad77d9aae85f92332c07df7fae441766a80277bb
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-16 12:24:33 +0200

    Add model Task to the models in the task_manager

--- task_manager/models.py
+++ task_manager/models.py
@@ -14,6 +14,19 @@ class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
 
+class Task(models.Model):
+    PRIORITY = ["Urgent and important",
+                "Urgent and not important",
+                "Not urgent and important",
+                "Not urgent and not important"]
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=255, choices=PRIORITY)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker)
+
 
 
 
