1d7a278d913fe69f62e1970f4f6a052166b0fae4
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-17 12:40:54 +0200

    Delete extra dash in path name task type create in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -22,7 +22,7 @@ from .views import (index,
 urlpatterns = [
     path("", index, name="index"),
     path("task_type/", TaskTypeListView.as_view(), name="task_type_list"),
-    path("task_type/create/", TaskTypeCreateView.as_view(), name="task_type_create_"),
+    path("task_type/create/", TaskTypeCreateView.as_view(), name="task_type_create"),
     path("task_type/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task_type_update"),
     path("task_type/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task_type_delete"),
     path("position/", PositionListView.as_view(), name="position_list"),
