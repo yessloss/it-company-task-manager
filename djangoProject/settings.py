e3db5df864bcf795bffa38600f5e6d53c6f83a7d
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-18 13:37:55 +0200

    Add auth user model and login redirect url in the settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_manager.Worker"
+
+LOGIN_REDIRECT_URL = "/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
