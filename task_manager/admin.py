4c3cb46a425c0eb92ef69982c31ee4b03da309fb
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-18 13:05:18 +0200

    Add register for all models in admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Worker, TaskType, Position, Task
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
+admin.site.register(Task)
