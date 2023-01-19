6afe61f6f080ab45c4e7bc8336a4b1a5193f9228
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-19 13:39:32 +0200

    Add assets root in the settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASSETS_ROOT = "/static/assets"
