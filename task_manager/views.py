2efaf836c7e9a6e68107eec59b580d2617c30248
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-19 09:45:15 +0200

    Add fields in task update view in the views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -144,6 +144,7 @@ class WorkerDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Worker
+    fields = "__all__"
     success_url = reverse_lazy("task_manager:worker_list")
 
 
@@ -178,6 +179,7 @@ class TaskCreateView(LoginRequiredMixin, generic.CreateView):
 
 class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
+    fields = "__all__"
     success_url = reverse_lazy("task_manager:task_list")
 
 
