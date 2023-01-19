9d34032a44a7d9891f6cede545f0e5c6c82c9d69
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-19 09:41:01 +0200

    Update priority in task model in the models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -28,19 +28,23 @@ class Worker(AbstractUser):
         return f"{self.username} ({self.first_name} {self.last_name})"
 
     def get_absolute_url(self):
-        return reverse("task_manager:worker_detail", kwargs={"pk": self.pk})
+        return reverse("task_manager:worker_detail", kwargs={"pk": str(self.pk)})
+
+
+PRIORITY = (
+        ("1", "Urgent and important"),
+        ("2", "Urgent and not important"),
+        ("3", "Not urgent and important"),
+        ("4", "Not urgent and not important"),
+    )
 
 
 class Task(models.Model):
-    PRIORITY = [(1, "Urgent and important"),
-                (2, "Urgent and not important"),
-                (3, "Not urgent and important"),
-                (4, "Not urgent and not important"),]
     name = models.CharField(max_length=255)
     description = models.TextField()
     deadline = models.DateField()
     is_completed = models.BooleanField()
-    priority = models.CharField(max_length=255, choices=PRIORITY)
+    priority = models.CharField(max_length=255, choices=PRIORITY, default="4")
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
     assignees = models.ManyToManyField(Worker, related_name="staff")
 
