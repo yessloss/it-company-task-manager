476b03e75175730abc6580d6fbcdd937ea8253a7
Andrew Losev <szghhgh@MacBook-Air.local>
2023-01-30 09:54:29 +0200

    Update SECRET_KEY in the settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '5uz2pe+$*)wjopw2!a64(z8n@6&2rh368zc^cqbs4&qzul1gc-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
